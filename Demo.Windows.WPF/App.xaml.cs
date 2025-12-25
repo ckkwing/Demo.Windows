5dd1daf21872fb87e7b98557fad7a6e56d596f74
Eric Chen <echen@nero.com>
2025-12-25 13:34:54 +0800

    Add "DynamicStringResourceWithStringFormatExtension"

--- Demo.Windows.WPF/App.xaml.cs
+++ Demo.Windows.WPF/App.xaml.cs
@@ -13,5 +13,14 @@ namespace Demo.Windows.WPF
     /// </summary>
     public partial class App : Application
     {
+        override protected void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Initialize the theme initializer
+            Theme.Initializer.Instance.Initialize("Demo.Windows.WPF");
+            Current.Resources.MergedDictionaries.Add(Theme.Resources.ThemeResources.LanguageResourceDictionary);
+        }
     }
+
+    
 }
